01323e29104f8b7ea803b1e1b816a8f563d9d5a5
OIITuMuCT <vataman@live.ru>
2026-07-10 17:02:13 +0300

    feat(courses): add students many-to-many field

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.template.loader import render_to_string
 
 from .fields import OrderField
 
@@ -66,6 +67,10 @@ class Course(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    students = models.ManyToManyField(
+        User, related_name='courses_joined',
+        blank=True
+    )
 
     class Meta:
         verbose_name = 'Курс'
@@ -152,6 +157,12 @@ class ItemBase(models.Model):
 
     def __str__(self):
         return self.title
+
+    def render(self):
+        return render_to_string(
+            f'courses/content/{self._meta.model_name}.html',
+            {'item': self}
+        )
 
 class Text(ItemBase):
     """Модель Text для хранения тестового содержимого. """
