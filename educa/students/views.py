c9486d94aebab60d97f5df2f3216c9cc1f698b05
OIITuMuCT <vataman@live.ru>
2026-07-10 17:06:06 +0300

    feat(students): add views Entoll, List, Detail

--- educa/students/views.py
+++ educa/students/views.py
@@ -1,8 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, FormView
+from django.views.generic.list import ListView
+
+from students.forms import CourseEnrollForm
+from courses.models import Course
+
 
 class StudentRegistrationView(CreateView):
     """Это представление `StudentRegistrationView`
@@ -20,3 +27,50 @@ class StudentRegistrationView(CreateView):
         )
         login(self.request, user)
         return result
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    """Это представление StudentEnrollCourseView,
+
+    Оно обслуживает зачисляемых студентов на курсы.
+    """
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail', args=[self.course.id])
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    """Это представление StudentCourseListView,
+
+    показывает курсы на которые зачислены студенты.
+    """
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+class StudentCourseDetailView(LoginRequiredMixin, DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            context['module'] = course.modules.get(
+                id=self.kwargs['module_id']
+            )
+        else:
+            context['module'] = course.modules.all()[0]
+        return context
