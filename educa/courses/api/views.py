04c2eec31982c888da8bcf5bde6c086682a93b3e
OIITuMuCT <vataman@live.ru>
2026-08-24 20:09:09 +0300

    feat(educa): add router courses, subjects

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,8 +1,9 @@
 from django.db.models import Count
 from rest_framework import generics
+from rest_framework import viewsets
 from courses.api.pagination import StandardPagination
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.models import Course, Subject
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
@@ -13,3 +14,12 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
 
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related('modules')
+    serializer_class = CourseSerializer
+    pagination_class = StandardPagination
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Subject.objects.annotate(total_courses=Count('courses'))
+    serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
