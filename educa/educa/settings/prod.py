e1d8d24cf53612b81b648bfd1511d2add5658493
OIITuMuCT <vataman@live.ru>
2026-09-03 19:37:14 +0300

    feat(educa): add cache: redis

--- educa/educa/settings/prod.py
+++ educa/educa/settings/prod.py
@@ -19,3 +19,6 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+REDIS_URL = 'redis://cache:6379'
+CACHES['default']['LOCATION'] = REDIS_URL
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
